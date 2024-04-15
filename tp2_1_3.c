88a3c8d8f01f3aa94ce3ef68c63c51d25d37139f
Nicholas1000200 <99295467+Nicholas1000200@users.noreply.github.com>
2024-04-15 18:52:30 -0300

    Correccion tercer commit tp2_1_3.c

--- tp2_1_3.c
+++ tp2_1_3.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #define N 5
 #define M 7
 
 int main()
 {
-    int i, j;
     int mt[N][M];
-    for (i = 0; i < N; i++)
+    int (*ptr)[M] = mt;
+
+    for (int i = 0; i < N; i++)
     {
-        for (j = 0; j < M; j++)
+        for (int j = 0; j < M; j++)
         {
-            mt[i][j] = 1 + rand() % 100;
-            printf("%lf", mt[i][j]);
+            *(*(ptr + i) + j) = 1 + rand() % 100;
+            printf("%d ", *(*(ptr + i) + j));
         }
         printf("\n");
     }
+
+    return 0;
 }
